8af352c3112e5e2cec35b7747bb0081aa8f17626
Himanshu Yengal <Himanshu.yengal@gmail.com>
2023-12-28 14:15:01 +0530

    added the remaining sections of the app like services, footer, tours and made a small change in data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import About from "./components/About";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Services />
+      <Tours />
+      <Footer />
     </>
   );
 }
